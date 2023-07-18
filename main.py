77f1bd93c70e93448bc5397ad6f942ca312db2ea
ildarishe <53740638+ildarishe@users.noreply.github.com>
2023-07-18 13:39:47 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 import requests
 
-cities = ["Лондон", "Шереметьево", "Череповец"]
-for city in cities:
-    url = f"https://wttr.in/{city}?nTqM&lang=ru"
-    response = requests.request(method="GET", url=url)
-    print(response.text)
+
+def make_request(city):
+    url = f"https://wttr.in/{city}"
+    payload = {"n": "", "T" : "", "q": "", "M": "", "lang": "ru"}
+    try:
+        response = requests.request(method="GET", url=url, params=payload)
+        if response.status_code == 200:
+            print(response.text)
+        else:
+            print("Упс... Что-то сломалось или в запросе есть ошибки")
+    except:
+        print("Сервер недоступен или в запросе есть ошибки")
+
+
+if __name__ == '__main__':
+    cities = ["Лондон", "Аэропорт Шереметьево", "Череповец"]
+    for city in cities:
+        make_request(city)
