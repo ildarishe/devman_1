2fc46027386c2ceaaeab9459f08a01305b926fb9
ildarishe <53740638+ildarishe@users.noreply.github.com>
2023-07-19 08:10:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,22 @@
 import requests
 
 
-def make_request(city):
+def get_forecast(city):
     url = f"https://wttr.in/{city}"
     payload = {"n": "", "T" : "", "q": "", "M": "", "lang": "ru"}
-    try:
-        response = requests.request(method="GET", url=url, params=payload)
-        if response.status_code == 200:
-            print(response.text)
-        else:
-            print("Упс... Что-то сломалось или в запросе есть ошибки")
-    except:
-        print("Сервер недоступен или в запросе есть ошибки")
+    response = requests.request(method="GET", url=url, params=payload)
+    if response.ok:
+        return response.text
+    else:
+        return "Упс... Что-то сломалось или в запросе есть ошибки"
+
+
 
 
 if __name__ == '__main__':
     cities = ["Лондон", "Аэропорт Шереметьево", "Череповец"]
     for city in cities:
-        make_request(city)
+        try:
+            print(get_forecast(city))
+        except:
+            print("Сервер недоступен или в запросе есть ошибки")
